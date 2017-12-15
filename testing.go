253ff522a60aae222f508be8e0c90c768c11e929
Jqs7 <7@jqs7.com>
2017-12-15 14:59:56 +0800

    add docs

--- testing.go
+++ testing.go
@@ -2,27 +2,32 @@ package biu
 
 import "github.com/levigross/grequests"
 
+// CT is a controller testing object.
 type CT struct {
-	ctl CtlInterface
+	ctlFuncs CtlFuncs
+	ctl      CtlInterface
 }
 
+// NewCT creates a controller testing object.
 func NewCT(ctl CtlInterface) *CT {
-	return &CT{ctl: ctl}
+	return &CT{ctl: ctl, ctlFuncs: GetCtlFuncs(ctl)}
 }
 
+// Handler creates a request for testing handler.
 func (t CT) Handler(method, path string,
 	option *grequests.RequestOptions) (*grequests.Response, error) {
-	return reqPathHandler(t.ctl, method, path, path, option)
+	return reqPathHandler(t.ctlFuncs, method, path, path, option)
 }
 
+// PathHandler creates a request for testing handler with path parameter.
 func (t CT) PathHandler(method, path string, reqPath string,
 	option *grequests.RequestOptions) (*grequests.Response, error) {
-	return reqPathHandler(t.ctl, method, path, reqPath, option)
+	return reqPathHandler(t.ctlFuncs, method, path, reqPath, option)
 }
 
-func reqPathHandler(ctl CtlInterface, method, path, reqPath string,
+func reqPathHandler(ctlFuncs CtlFuncs, method, path, reqPath string,
 	option *grequests.RequestOptions) (*grequests.Response, error) {
-	testServer := GetCtlFuncs(ctl).NewTestServer(method, path)
+	testServer := ctlFuncs.NewTestServer(method, path)
 	defer testServer.Close()
 	return grequests.Req(method, testServer.URL+reqPath, option)
 }
