3ac33b541b773083c95222912cc9266f1ce124fc
Jqs7 <7@jqs7.com>
2018-04-17 09:19:02 +0800

    add docs for parameter

--- parameter.go
+++ parameter.go
@@ -8,11 +8,13 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Parameter wrap parameter value in a request.
 type Parameter struct {
 	Value []string
 	error
 }
 
+// Bool converts a parameter value to bool.
 func (p Parameter) Bool() (bool, error) {
 	var zeroVal bool
 	if p.error != nil {
@@ -24,6 +26,7 @@ func (p Parameter) Bool() (bool, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// BoolDefault converts a parameter value to bool with default value.
 func (p Parameter) BoolDefault(defaultValue bool) bool {
 	rst, err := p.Bool()
 	if err != nil {
@@ -32,6 +35,7 @@ func (p Parameter) BoolDefault(defaultValue bool) bool {
 	return rst
 }
 
+// BoolArray converts a parameter value to bool array.
 func (p Parameter) BoolArray() ([]bool, error) {
 	var rst []bool
 	if p.error != nil {
@@ -48,6 +52,7 @@ func (p Parameter) BoolArray() ([]bool, error) {
 	return rst, nil
 }
 
+// Float32 converts a parameter value to float32.
 func (p Parameter) Float32() (float32, error) {
 	var zeroVal float32
 	if p.error != nil {
@@ -60,6 +65,7 @@ func (p Parameter) Float32() (float32, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Float32Default converts a parameter value to float32 with default value.
 func (p Parameter) Float32Default(defaultValue float32) float32 {
 	rst, err := p.Float32()
 	if err != nil {
@@ -68,6 +74,7 @@ func (p Parameter) Float32Default(defaultValue float32) float32 {
 	return rst
 }
 
+// Float32Array converts a parameter value to float32 array.
 func (p Parameter) Float32Array() ([]float32, error) {
 	var rst []float32
 	if p.error != nil {
@@ -84,6 +91,7 @@ func (p Parameter) Float32Array() ([]float32, error) {
 	return rst, nil
 }
 
+// Float64 converts a parameter value to float64.
 func (p Parameter) Float64() (float64, error) {
 	var zeroVal float64
 	if p.error != nil {
@@ -95,6 +103,7 @@ func (p Parameter) Float64() (float64, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Float64Default converts a parameter value to float64 with default value.
 func (p Parameter) Float64Default(defaultValue float64) float64 {
 	rst, err := p.Float64()
 	if err != nil {
@@ -103,6 +112,7 @@ func (p Parameter) Float64Default(defaultValue float64) float64 {
 	return rst
 }
 
+// Float64Array converts a parameter value to float64 array.
 func (p Parameter) Float64Array() ([]float64, error) {
 	var rst []float64
 	if p.error != nil {
@@ -119,6 +129,7 @@ func (p Parameter) Float64Array() ([]float64, error) {
 	return rst, nil
 }
 
+// Int converts a parameter value to int.
 func (p Parameter) Int() (int, error) {
 	var zeroVal int
 	if p.error != nil {
@@ -131,6 +142,7 @@ func (p Parameter) Int() (int, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// IntDefault converts a parameter value to int with default value.
 func (p Parameter) IntDefault(defaultValue int) int {
 	rst, err := p.Int()
 	if err != nil {
@@ -139,6 +151,7 @@ func (p Parameter) IntDefault(defaultValue int) int {
 	return rst
 }
 
+// IntArray converts a parameter value to int array.
 func (p Parameter) IntArray() ([]int, error) {
 	var rst []int
 	if p.error != nil {
@@ -155,6 +168,7 @@ func (p Parameter) IntArray() ([]int, error) {
 	return rst, nil
 }
 
+// Int8 converts a parameter value to int8.
 func (p Parameter) Int8() (int8, error) {
 	var zeroVal int8
 	if p.error != nil {
@@ -167,6 +181,7 @@ func (p Parameter) Int8() (int8, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Int8Default converts a parameter value to int8 with default value.
 func (p Parameter) Int8Default(defaultValue int8) int8 {
 	rst, err := p.Int8()
 	if err != nil {
@@ -175,6 +190,7 @@ func (p Parameter) Int8Default(defaultValue int8) int8 {
 	return rst
 }
 
+// Int8Array converts a parameter value to int8 array.
 func (p Parameter) Int8Array() ([]int8, error) {
 	var rst []int8
 	if p.error != nil {
@@ -191,6 +207,7 @@ func (p Parameter) Int8Array() ([]int8, error) {
 	return rst, nil
 }
 
+// Int16 converts a parameter value to int16.
 func (p Parameter) Int16() (int16, error) {
 	var zeroVal int16
 	if p.error != nil {
@@ -203,6 +220,7 @@ func (p Parameter) Int16() (int16, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Int16Default converts a parameter value to int16 with default value.
 func (p Parameter) Int16Default(defaultValue int16) int16 {
 	rst, err := p.Int16()
 	if err != nil {
@@ -211,6 +229,7 @@ func (p Parameter) Int16Default(defaultValue int16) int16 {
 	return rst
 }
 
+// Int16Array converts a parameter value to int16 array.
 func (p Parameter) Int16Array() ([]int16, error) {
 	var rst []int16
 	if p.error != nil {
@@ -227,6 +246,7 @@ func (p Parameter) Int16Array() ([]int16, error) {
 	return rst, nil
 }
 
+// Int32 converts a parameter value to int32.
 func (p Parameter) Int32() (int32, error) {
 	var zeroVal int32
 	if p.error != nil {
@@ -239,6 +259,7 @@ func (p Parameter) Int32() (int32, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Int32Default converts a parameter value to int32 with default value.
 func (p Parameter) Int32Default(defaultValue int32) int32 {
 	rst, err := p.Int32()
 	if err != nil {
@@ -247,6 +268,7 @@ func (p Parameter) Int32Default(defaultValue int32) int32 {
 	return rst
 }
 
+// Int32Array converts a parameter value to int32 array.
 func (p Parameter) Int32Array() ([]int32, error) {
 	var rst []int32
 	if p.error != nil {
@@ -276,6 +298,7 @@ func strToInt64(s string) (int64, error) {
 	return v, err
 }
 
+// Int64 converts a parameter value to int64.
 func (p Parameter) Int64() (int64, error) {
 	var zeroVal int64
 	if p.error != nil {
@@ -287,6 +310,7 @@ func (p Parameter) Int64() (int64, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Int64Default converts a parameter value to int64 with default value.
 func (p Parameter) Int64Default(defaultValue int64) int64 {
 	rst, err := p.Int64()
 	if err != nil {
@@ -295,6 +319,7 @@ func (p Parameter) Int64Default(defaultValue int64) int64 {
 	return rst
 }
 
+// Int64Array converts a parameter value to int64 array.
 func (p Parameter) Int64Array() ([]int64, error) {
 	var rst []int64
 	if p.error != nil {
@@ -311,6 +336,7 @@ func (p Parameter) Int64Array() ([]int64, error) {
 	return rst, nil
 }
 
+// Uint converts a parameter value to uint.
 func (p Parameter) Uint() (uint, error) {
 	var zeroVal uint
 	if p.error != nil {
@@ -323,6 +349,7 @@ func (p Parameter) Uint() (uint, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// UintDefault converts a parameter value to uint with default value.
 func (p Parameter) UintDefault(defaultValue uint) uint {
 	rst, err := p.Uint()
 	if err != nil {
@@ -331,6 +358,7 @@ func (p Parameter) UintDefault(defaultValue uint) uint {
 	return rst
 }
 
+// UintArray converts a parameter value to uint array.
 func (p Parameter) UintArray() ([]uint, error) {
 	var rst []uint
 	if p.error != nil {
@@ -347,6 +375,7 @@ func (p Parameter) UintArray() ([]uint, error) {
 	return rst, nil
 }
 
+// Uint8 converts a parameter value to uint8.
 func (p Parameter) Uint8() (uint8, error) {
 	var zeroVal uint8
 	if p.error != nil {
@@ -359,6 +388,7 @@ func (p Parameter) Uint8() (uint8, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Uint8Default converts a parameter value to uint8 with default value.
 func (p Parameter) Uint8Default(defaultValue uint8) uint8 {
 	rst, err := p.Uint8()
 	if err != nil {
@@ -367,6 +397,7 @@ func (p Parameter) Uint8Default(defaultValue uint8) uint8 {
 	return rst
 }
 
+// Uint8Array converts a parameter value to uint8 array.
 func (p Parameter) Uint8Array() ([]uint8, error) {
 	var rst []uint8
 	if p.error != nil {
@@ -383,6 +414,7 @@ func (p Parameter) Uint8Array() ([]uint8, error) {
 	return rst, nil
 }
 
+// Uint16 converts a parameter value to uint16.
 func (p Parameter) Uint16() (uint16, error) {
 	var zeroVal uint16
 	if p.error != nil {
@@ -395,6 +427,7 @@ func (p Parameter) Uint16() (uint16, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Uint16Default converts a parameter value to uint16 with default value.
 func (p Parameter) Uint16Default(defaultValue uint16) uint16 {
 	rst, err := p.Uint16()
 	if err != nil {
@@ -403,6 +436,7 @@ func (p Parameter) Uint16Default(defaultValue uint16) uint16 {
 	return rst
 }
 
+// Uint16Array converts a parameter value to uint16 array.
 func (p Parameter) Uint16Array() ([]uint16, error) {
 	var rst []uint16
 	if p.error != nil {
@@ -419,6 +453,7 @@ func (p Parameter) Uint16Array() ([]uint16, error) {
 	return rst, nil
 }
 
+// Uint32 converts a parameter value to uint32.
 func (p Parameter) Uint32() (uint32, error) {
 	var zeroVal uint32
 	if p.error != nil {
@@ -431,6 +466,7 @@ func (p Parameter) Uint32() (uint32, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Uint32Default converts a parameter value to uint32 with default value.
 func (p Parameter) Uint32Default(defaultValue uint32) uint32 {
 	rst, err := p.Uint32()
 	if err != nil {
@@ -439,6 +475,7 @@ func (p Parameter) Uint32Default(defaultValue uint32) uint32 {
 	return rst
 }
 
+// Uint32Array converts a parameter value to uint32 array.
 func (p Parameter) Uint32Array() ([]uint32, error) {
 	var rst []uint32
 	if p.error != nil {
@@ -468,6 +505,7 @@ func strToUint64(s string) (uint64, error) {
 	return v, err
 }
 
+// Uint64 converts a parameter value to uint64.
 func (p Parameter) Uint64() (uint64, error) {
 	var zeroVal uint64
 	if p.error != nil {
@@ -479,6 +517,7 @@ func (p Parameter) Uint64() (uint64, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// Uint64Default converts a parameter value to uint64 with default value.
 func (p Parameter) Uint64Default(defaultValue uint64) uint64 {
 	rst, err := p.Uint64()
 	if err != nil {
@@ -487,6 +526,7 @@ func (p Parameter) Uint64Default(defaultValue uint64) uint64 {
 	return rst
 }
 
+// Uint64Array converts a parameter value to uint64 array.
 func (p Parameter) Uint64Array() ([]uint64, error) {
 	var rst []uint64
 	if p.error != nil {
@@ -503,6 +543,7 @@ func (p Parameter) Uint64Array() ([]uint64, error) {
 	return rst, nil
 }
 
+// String converts a parameter value to string.
 func (p Parameter) String() (string, error) {
 	var zeroVal string
 	if p.error != nil {
@@ -514,6 +555,7 @@ func (p Parameter) String() (string, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// StringDefault converts a parameter value to string with default value.
 func (p Parameter) StringDefault(defaultValue string) string {
 	rst, err := p.String()
 	if err != nil {
@@ -522,6 +564,7 @@ func (p Parameter) StringDefault(defaultValue string) string {
 	return rst
 }
 
+// StringArray converts a parameter value to string array.
 func (p Parameter) StringArray() ([]string, error) {
 	var rst []string
 	if p.error != nil {
@@ -530,6 +573,7 @@ func (p Parameter) StringArray() ([]string, error) {
 	return p.Value, nil
 }
 
+// Time parse a parameter value to time.Time with given layout.
 func (p Parameter) Time(layout string) (time.Time, error) {
 	var zeroVal time.Time
 	if p.error != nil {
@@ -541,6 +585,7 @@ func (p Parameter) Time(layout string) (time.Time, error) {
 	return zeroVal, errors.New("parameter is empty")
 }
 
+// TimeDefault parse a parameter value to time.Time with given layout with default value.
 func (p Parameter) TimeDefault(layout string, defaultValue time.Time) time.Time {
 	rst, err := p.Time(layout)
 	if err != nil {
@@ -549,6 +594,7 @@ func (p Parameter) TimeDefault(layout string, defaultValue time.Time) time.Time 
 	return rst
 }
 
+// TimeArray parse a parameter value to time.Time array.
 func (p Parameter) TimeArray(layout string) ([]time.Time, error) {
 	var rst []time.Time
 	if p.error != nil {
