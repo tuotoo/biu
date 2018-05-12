b5c2a1646fb35caa09d91f54ce4cec34f83b46dc
Jqs7 <7@jqs7.com>
2018-05-12 22:38:02 +0800

    test($parameter): add test for parameter
    and if to array has error, it should return a nil slice

---
+++ parameter_test.go
@@ -0,0 +1,238 @@
+package biu
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParameter_Bool(t *testing.T) {
+	type fields struct {
+		Value []string
+		error error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "parameter is nil",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: nil},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "parameter is 1",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"1"}},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "parameter is 0",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"0"}},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name: "parameter is 2",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"2"}},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "parameter is A",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"A"}},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "parameter is true",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"true"}},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "parameter is True",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"True"}},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "parameter is TRue",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"TRue"}},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "parameter is false",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"false"}},
+			want:    false,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Parameter{
+				Value: tt.fields.Value,
+				error: tt.fields.error,
+			}
+			got, err := p.Bool()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parameter.Bool() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Parameter.Bool() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParameter_BoolDefault(t *testing.T) {
+	type fields struct {
+		Value []string
+		error error
+	}
+	type args struct {
+		defaultValue bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "parameter is nil",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: nil},
+			args: struct{ defaultValue bool }{defaultValue: true},
+			want: true,
+		},
+		{
+			name: "parameter is 1",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"1"}},
+			args: struct{ defaultValue bool }{defaultValue: true},
+			want: true,
+		},
+		{
+			name: "parameter is 0",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"0"}},
+			args: struct{ defaultValue bool }{defaultValue: true},
+			want: false,
+		},
+		{
+			name: "parameter is 2",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"2"}},
+			args: struct{ defaultValue bool }{defaultValue: true},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Parameter{
+				Value: tt.fields.Value,
+				error: tt.fields.error,
+			}
+			if got := p.BoolDefault(tt.args.defaultValue); got != tt.want {
+				t.Errorf("Parameter.BoolDefault() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParameter_BoolArray(t *testing.T) {
+	type fields struct {
+		Value []string
+		error error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []bool
+		wantErr bool
+	}{
+		{
+			name: "parameter is nil",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: nil},
+			want:    []bool{},
+			wantErr: false,
+		},
+		{
+			name: "parameter is [true, 0]",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"true", "0"}},
+			want:    []bool{true, false},
+			wantErr: false,
+		},
+		{
+			name: "parameter include invalid value",
+			fields: struct {
+				Value []string
+				error error
+			}{Value: []string{"true", "a"}},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Parameter{
+				Value: tt.fields.Value,
+				error: tt.fields.error,
+			}
+			got, err := p.BoolArray()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parameter.BoolArray() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parameter.BoolArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
