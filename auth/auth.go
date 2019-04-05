0f8986bc8023144e1c93987a4a79ac3f3be1984c
Jqs7 <7@jqs7.com>
2019-04-05 12:43:51 +0800

    feat(auth): multi instance support for auth

--- auth/auth.go
+++ auth/auth.go
@@ -9,31 +9,54 @@ import (
 	"github.com/tuotoo/biu/log"
 )
 
-var (
-	JWTTimeout = time.Minute * 5
-	JWTSecret  = func(userID string) (secret []byte, err error) {
+type Instance struct {
+	Timeout        time.Duration
+	RefreshTimeout time.Duration
+	SecretFunc     func(string) ([]byte, error)
+}
+
+var DefaultInstance = &Instance{
+	Timeout: time.Minute * 5,
+	SecretFunc: func(userID string) (secret []byte, err error) {
 		return []byte("secret"), nil
-	}
-	JWTRefreshTimeout = time.Hour * 24 * 7
-)
+	},
+	RefreshTimeout: time.Hour * 24 * 7,
+}
 
 // Sign returns a signed jwt string.
-func Sign(userID string) (token string, err error) {
+func (i *Instance) Sign(userID string) (token string, err error) {
 	now := time.Now()
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"uid": userID,
-		"exp": now.Add(JWTTimeout).Unix(),
+		"exp": now.Add(i.Timeout).Unix(),
 		"iat": now.Unix(),
 	})
-	sec, err := JWTSecret(userID)
+	sec, err := i.SecretFunc(userID)
 	if err != nil {
 		return "", err
 	}
 	return jwtToken.SignedString(sec)
 }
 
+func JWTTimeout(timeout time.Duration) {
+	DefaultInstance.Timeout = timeout
+}
+
+func JWTSecret(f func(string) ([]byte, error)) {
+	DefaultInstance.SecretFunc = f
+}
+
+func JWTRefreshTimeout(timeout time.Duration) {
+	DefaultInstance.RefreshTimeout = timeout
+}
+
+// Sign returns a signed jwt string with default instance.
+func Sign(userID string) (token string, err error) {
+	return DefaultInstance.Sign(userID)
+}
+
 // ParseToken parse a token string.
-func ParseToken(token string) (*jwt.Token, error) {
+func (i *Instance) ParseToken(token string) (*jwt.Token, error) {
 	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		if _, methodOK := token.Method.(*jwt.SigningMethodHMAC); !methodOK {
 			signingErr := fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
@@ -52,13 +75,18 @@ func ParseToken(token string) (*jwt.Token, error) {
 			log.Info().Err(uidErr).Msg("parse uid in token")
 			return nil, uidErr
 		}
-		return JWTSecret(uid)
+		return i.SecretFunc(uid)
 	})
+}
+
+// ParseToken parse a token string with default instance.
+func ParseToken(token string) (*jwt.Token, error) {
+	return DefaultInstance.ParseToken(token)
 }
 
 // RefreshToken accepts a valid token and
 // returns a new token with new expire time.
-func RefreshToken(token string) (newToken string, err error) {
+func (i *Instance) RefreshToken(token string) (newToken string, err error) {
 	t, err := ParseToken(token)
 	if err != nil {
 		log.Info().Err(err).Msg("parse token")
@@ -74,7 +102,7 @@ func RefreshToken(token string) (newToken string, err error) {
 	}
 	now := time.Now()
 	iat := int64(iatF64)
-	if iat < now.Add(-JWTRefreshTimeout).Unix() {
+	if iat < now.Add(-i.RefreshTimeout).Unix() {
 		return "", errors.New("refresh is expired")
 	}
 	uid, ok := claims["uid"].(string)
@@ -83,18 +111,24 @@ func RefreshToken(token string) (newToken string, err error) {
 	}
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"uid": uid,
-		"exp": now.Add(JWTTimeout).Unix(),
+		"exp": now.Add(i.Timeout).Unix(),
 		"iat": iat,
 	})
-	sec, err := JWTSecret(uid)
+	sec, err := i.SecretFunc(uid)
 	if err != nil {
 		return "", err
 	}
 	return jwtToken.SignedString(sec)
 }
 
+// RefreshToken accepts a valid token and
+// returns a new token with new expire time.
+func RefreshToken(token string) (newToken string, err error) {
+	return DefaultInstance.RefreshToken(token)
+}
+
 // CheckToken accept a jwt token and returns the uid in token.
-func CheckToken(token string) (userID string, err error) {
+func (i *Instance) CheckToken(token string) (userID string, err error) {
 	t, err := ParseToken(token)
 	if err != nil {
 		log.Info().Err(err).Msg("parse token")
@@ -109,4 +143,8 @@ func CheckToken(token string) (userID string, err error) {
 		return "", errors.New("not available uid")
 	}
 	return uid, nil
+}
+
+func CheckToken(token string) (userID string, err error) {
+	return DefaultInstance.CheckToken(token)
 }
